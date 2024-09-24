a56acfdc7ecb63836565686bce60de8c0e6749fb
ksp0814 <rkdtpvlf456@naver.com>
2024-09-24 15:16:24 +0900

    first

---
+++ src/main/java/dev/auth/GooglettemyApplication.java
@@ -0,0 +1,13 @@
+package dev.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglettemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglettemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/auth/GooglettemyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooglettemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
